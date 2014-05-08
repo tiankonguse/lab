b2298ac5f011a13cc27490328643a060cd859fe0
tiankonguse <i@tiankonguse.com>
2014-05-08 11:13:45 +0800

    update

--- kirichik/js/all.js
+++ kirichik/js/all.js
@@ -1,6 +1,19 @@
 // Home page hide
 //---------------------------------------------
 $(document).ready(function(){
+
+    function getAnim(){
+        var data = "bounce flash pulse rubberBand shake swing tada wobble bounceIn bounceInDown bounceInLeft bounceInRight bounceInUp bounceOut bounceOutDown bounceOutLeft bounceOutRight bounceOutUp fadeIn fadeInDown fadeInDownBig fadeInLeft fadeInLeftBig fadeInRight fadeInRightBig fadeInUp fadeInUpBig fadeOut fadeOutDown fadeOutDownBig fadeOutLeft fadeOutLeftBig fadeOutRight fadeOutRightBig fadeOutUp fadeOutUpBig flip flipInX flipInY flipOutX flipOutY lightSpeedIn lightSpeedOut rotateIn rotateInDownLeft rotateInDownRight rotateInUpLeft rotateInUpRight rotateOut rotateOutDownLeft rotateOutDownRight rotateOutUpLeft rotateOutUpRight slideInDown slideInLeft slideInRight slideOutLeft slideOutRight slideOutUp hinge rollIn rollOut";
+        var obj = data.split(" ");
+        return obj[parseInt(Math.random()*obj.length)];
+    }
+
+    function testAnim(obj) {
+        var x = getAnim();
+        obj.addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            obj.removeClass(x + " animated");
+        });
+    };
     if (jQuery(window).width() > 1000) {
         $(".pg-item").css("min-height",($(window).height()) +"px");
         $(window).resize(function(){
@@ -9,6 +22,7 @@ $(document).ready(function(){
         var animate_in = 499, animate_out = 500;
         var lock = 0;
         var $pgItem = $(".pg-item");
+        var $home = $("#home");
         function page_up(){
             var id = $pgItem.filter(":in-viewport:last").attr("id") ;
             if(!id){
@@ -18,18 +32,24 @@ $(document).ready(function(){
                 var $pre = $id.prev();
                 if($pre.length != 0){
                     // not top
+                    testAnim($id);
+                    var top = $pre.offset().top;
+                    testAnim($pre);
                     lock = 1;
                     $("html, body").animate({
-                        scrollTop: ($pre.offset().top) + "px"
+                        scrollTop: top + "px"
                     }, animate_in, "easeInOutExpo", function(){
                         window.setTimeout(function(){
                             lock = 0;
                         }, animate_out);
+                        
                     });
                 }else{
+                    testAnim($id);
+                    testAnim($home);
                     lock = 1;
                     $("html, body").animate({
-                        scrollTop: (0) + "px"
+                        scrollTop: 0 + "px"
                     }, animate_in, "easeInOutExpo", function(){
                         window.setTimeout(function(){
                             lock = 0;
@@ -43,9 +63,13 @@ $(document).ready(function(){
             var id = $pgItem.filter(":in-viewport:last").attr("id") ;
             if(!id){
                 var $next =$($pgItem[0]);
+                testAnim($home);
+                var top = $next.offset().top;
+                testAnim($next);
+
                 lock = 1;
                 $("html, body").animate({
-                    scrollTop: ($next.offset().top) + "px"
+                    scrollTop: top + "px"
                 }, animate_in, "easeInOutQuad", function(){
                     window.setTimeout(function(){
                         lock = 0;
@@ -56,9 +80,12 @@ $(document).ready(function(){
                 var $id = $("#" + id);
                 var $next = $id.next();
                 if($next.length != 0){
+                    testAnim($id);
+                    var top = $next.offset().top;
+                    testAnim($next);
                     lock = 1;
                     $("html, body").animate({
-                        scrollTop: ($next.offset().top) + "px"
+                        scrollTop: top + "px"
                     }, animate_in, "easeInOutQuad", function(){
                         window.setTimeout(function(){
                             lock = 0;
@@ -85,7 +112,7 @@ $(document).ready(function(){
             return false;
         });
 
-        
+
 
         $(document).keydown(function(event){
             if(lock == 1){
@@ -154,7 +181,7 @@ $(document).ready(function(){
 
     $(".ps-icon-menu, .ps-menu-toggle").click(function(){
         if ($(".sidebar").hasClass("opened")) {
-        
+
             $(".sidebar").animate({
                 left: "-350px"
             }, "easeOutCirc");
@@ -165,10 +192,10 @@ $(document).ready(function(){
             });
             $(".white-overlay").fadeOut();
             $(".sidebar").removeClass("opened");
-            
+
         }
         else {
-        
+
             $(".white-overlay").fadeIn();
             $(".sidebar").animate({
                 left: 0
@@ -179,13 +206,13 @@ $(document).ready(function(){
                 marginRight: 0
             });
             $(".sidebar").addClass("opened");
-            
+
         }
     });
-    
+
     $(window).scroll(function(){
         if ($(".sidebar").hasClass("opened")) {
-        
+
             $(".sidebar").animate({
                 left: "-350px"
             }, "easeOutCirc");
@@ -196,9 +223,9 @@ $(document).ready(function(){
             });
             $(".white-overlay").fadeOut();
             $(".sidebar").removeClass("opened");
-            
+
         }
     });
-    
+
 });
 
